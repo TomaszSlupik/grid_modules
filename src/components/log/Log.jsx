72210ed6ecdd0cb72e90abd75e7c6499891687a9
TomaszSlupik <slupiktomasz@gmail.com>
2024-03-06 08:21:45 +0100

    log

---
+++ src/components/log/Log.jsx
@@ -0,0 +1,120 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import './Log.scss'; // Importujemy plik CSS dla stylów
+
+export default function Log() {
+  return (
+    <div>
+        <div class="outer">
+  <div class="inner">
+    <table>
+        <tr>
+          <th class="hard_left">Header A</th>
+          <th class="next_left">Header B</th>
+          <th>Header C</th>
+          <th>Header D</th>
+          <th>Header E</th>
+          <th>Header A</th>
+          <th>Header B</th>
+          <th>Header C</th>
+          <th>Header D</th>
+          <th>Header E</th>
+        </tr>
+        <tr>
+          <td class="hard_left">col 1 - A</td>
+          <td class="next_left">col 2 - A</td>
+          <td>col 3 - A</td>
+          <td>col 4 - A</td>
+          <td>col 5 - A</td>
+          <td>col 1 - A</td>
+          <td>col 2 - A</td>
+          <td>col 3 - A</td>
+          <td>col 4 - A</td>
+          <td>col 5 - A</td>
+        </tr>
+        <tr>
+          
+          <td class="hard_left">col 1 - B</td>
+          <td class="next_left">col 2 - B</td>
+          <td>col 3 - B</td>
+          <td>col 4 - B</td>
+          <td>col 5 - B</td>
+          <td>col 1 - A</td>
+          <td>col 2 - A</td>
+          <td>col 3 - A</td>
+          <td>col 4 - A</td>
+          <td>col 5 - A</td>
+        </tr>
+        <tr>
+          
+          <td class="hard_left">col 1 - C</td>
+          <td class="next_left">col 2 - C</td>
+          <td>col 3 - C</td>
+          <td>col 4 - C</td>
+          <td>col 5 - C</td>
+          <td>col 1 - A</td>
+          <td>col 2 - A</td>
+          <td>col 3 - A</td>
+          <td>col 4 - A</td>
+          <td>col 5 - A</td>
+        </tr>
+        <tr>
+          <td class="hard_left">col 1 - A</td>
+          <td class="next_left">col 2 - A</td>
+          <td>col 3 - A</td>
+          <td>col 4 - A</td>
+          <td>col 5 - A</td>
+          <td>col 1 - B</td>
+          <td>col 2 - B</td>
+          <td>col 3 - B</td>
+          <td>col 4 - B</td>
+          <td>col 5 - B</td>
+        </tr>
+        <tr>
+          <td class="hard_left">col 1 - A</td>
+          <td class="next_left">col 2 - A</td>
+          <td>col 3 - A</td>
+          <td>col 4 - A</td>
+          <td>col 5 - A</td>
+          <td>col 1 - C</td>
+          <td>col 2 - C</td>
+          <td>col 3 - C</td>
+          <td>col 4 - C</td>
+          <td>col 5 - C</td>
+        </tr>
+        <tr>
+          <td class="hard_left">col 1 - A</td>
+          <td class="next_left">col 2 - A</td>
+          <td>col 3 - A</td>
+          <td>col 4 - A</td>
+          <td>col 5 - A</td>
+          <td>col 1 - B</td>
+          <td>col 2 - B</td>
+          <td>col 3 - B</td>
+          <td>col 4 - B</td>
+          <td>col 5 - B</td>
+        </tr>
+        <tr>
+          <td class="hard_left">col 1 - A</td>
+          <td class="next_left">col 2 - A</td>
+          <td>col 3 - A</td>
+          <td>col 4 - A</td>
+          <td>col 5 - A</td>
+          <td>col 1 - C</td>
+          <td>col 2 - C</td>
+          <td>col 3 - C</td>
+          <td>col 4 - C</td>
+          <td>col 5 - C</td>
+        </tr>
+    </table>
+  </div>
+</div>
+    </div>
+  );
+}
