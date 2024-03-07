41d16bb2b2a4fbb44438713c776a16123f5be58b
TomaszSlupik <slupiktomasz@gmail.com>
2024-03-07 19:35:42 +0100

    change

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/home/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Loginuser from "./components/loginuser/Loginuser";
 import Flag from "./components/Flag/Flag";
+import Log from "./components/log/Log";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Loginuser />} />
           <Route path="/module" element={<Home />}></Route>
           <Route path="/flag" element={<Flag />}></Route>
+          <Route path="/log" element={<Log />} />
         </Routes>
       </Router>
     </div>
--- src/components/log/Log.jsx
+++ src/components/log/Log.jsx
@@ -1,12 +1,5 @@
 import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import './Log.scss'; // Importujemy plik CSS dla stylów
+import './Log.scss'; 
 
 export default function Log() {
   return (
